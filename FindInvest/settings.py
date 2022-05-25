daf163c009a2d615f74a90b5498c146c4746991d
Tom-michel <rufin.btompe@facsciences-uy1.cm>
2022-05-25 02:39:19 +0100

    Ajout du mdp_manager (Reset and Change password)

--- FindInvest/settings.py
+++ FindInvest/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from curses.ascii import EM
 from pathlib import Path
 import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,6 +31,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# EMAIL_FILE_PATH = BASE_DIR / 'emails'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'btmicheldada@gmail.com'
+EMAIL_HOST_PASSWORD = 'skqcluutwjkazjyd'
+EMAIL_PORT = 587
 
 # Application definition
 
@@ -124,6 +135,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,13 @@ from .views import *
 urlpatterns = [
     path('connexion', connexion, name="connexion"),
     path('déconnexion', user_logout, name="user_logout"),
+    path('reset_password', reset_pwd, name="reset_pwd"),
+    path('new_password', new_password, name="new_password"),
     path('register_student', register_student, name="register_student"),
     path('register_investor', register_investor, name="register_investor"),
-    path('update_profile/<str:id_e>', update_profile_student, name="update_profile_student"),
+    # path('update_profile/<str:id_e>', update_profile_student, name="update_profile_student"),
+    path('update_profile', update_profile_student, name="update_profile_student"),
     path('profile_student/<str:id_e>', profile_student, name="profile_student"),
-    path('update_profile_i/<str:id_i>', update_profile_investor, name="update_profile_investor"),
+    # path('update_profile_i/<str:id_i>', update_profile_investor, name="update_profile_investor"),
+    path('update_profile_i', update_profile_investor, name="update_profile_investor"),
 ]
