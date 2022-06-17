500c5a422b7b651681a1bb8c596f50d81b4d50c4
jeanpetitt <82118752+jeanpetitt@users.noreply.github.com>
2022-06-17 07:20:01 +0100

    not change

--- FindInvest/production_setting.py
+++ FindInvest/production_setting.py
@@ -7,6 +7,8 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware',]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 SECRET_KEY = 'uyp1wld474qu#gnfvtvl%yo93cvg_ko1*j-)py5*u(i5lmi3v%'
 
