313312a8d0509b326527e0c3b668693ac57c6717
jeanpetitt <82118752+jeanpetitt@users.noreply.github.com>
2022-07-10 23:21:46 +0100

    prise en compte des media

--- FindInvest/urls.py
+++ FindInvest/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static 
+from django.views.static import serve
 from django.contrib.auth import views
 
 urlpatterns = [
@@ -30,7 +31,9 @@ urlpatterns = [
     path('reset_password_complete', views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 
     path('accounts/', include('allauth.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT})
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    #urlpatterns += static(settings.STATIC_URL, document_root = settings.SATIC_ROOT)
 
